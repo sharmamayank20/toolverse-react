9d8686a08cbacad28dd6445a454a81ce52e9c553
tejasparmarr <tejasbro17@gmail.com>
2026-09-13 00:32:47 +0530

    Update vite.config.js
    Update 13 September

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/TOOLVERSE/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/TOOLVERSE/' : '/',
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
-})
+}))
